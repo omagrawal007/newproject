7f1ff1fee0afe3fea9081e460e5ed6ca89bfb7f9
unknown <om@vstglobal.com>
2018-02-20 16:23:05 +0530

    change config

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,8 @@ module.exports = {
   version:'0.0.1',
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost:27017/timesheet',
+    // uri: 'mongodb://localhost:27017/timesheet',
+    uri: 'mongodb://om.agrawal9415@gmail.com:omagrawal007@ds143738.mlab.com:43738/timesheet1231',
   },
   email:{
     host    : 'mail.vstglobal.com',
